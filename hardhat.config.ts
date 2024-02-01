8126c5889a620482c473136b6823a7e254602b8d
Ross <therossgalloway@gmail.com>
2024-02-01 13:48:28 -0500

    update solidity compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
         },
       },
       {
-        version: "0.8.0",
+        version: "0.8.1",
         settings: {
           optimizer: {
             enabled: true,
